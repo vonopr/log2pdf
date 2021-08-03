0320d5d58bf942f4ec723cf57a8f8a3c522db9ea
vonopr <vonopr@ya.ru>
2021-08-03 12:26:26 +0300

    Improve script's portability by qouting strings
    Quotes around string arguments of the script's commands
    improve the portability.
    
    Now I can execute the script in Windows OS just by copying
    the content log2git.sh into cmd.

--- log2pdf.sh
+++ log2pdf.sh
@@ -1,8 +1,8 @@
-echo \`\`\` > gitlog.md
-git log --reverse --stat --format=\`\`\`%n##\ %s%n%n\`\`\`%n%n%H%n%ai%n%an\ \<%ae\>%n\`\`\`%n\ \ %b%nFiles\ changed:%n\`\`\` >> gitlog.md
-echo \`\`\` >> gitlog.md
-echo \\newpage >> gitlog.md
-echo \\tableofcontents\\label{toc} >> gitlog.md
+echo '```' > gitlog.md
+git log --reverse --stat --format='```%n## %s%n%n```%n%n%H%n%ai%n%an <%ae>%n```%n  %b%nFiles changed: %n```' >> gitlog.md
+echo '```' >> gitlog.md
+echo '\newpage' >> gitlog.md
+echo '\tableofcontents\label{toc}' >> gitlog.md
 
 pandoc -s  gitlog.yaml gitlog.md  -o gitlog.tex
 latexmk -pdf -pdfxe -f  gitlog.tex
