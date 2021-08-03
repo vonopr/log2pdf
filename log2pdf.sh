31853c295a1b12cb91f8e2c0a906500dfcf43860
Vladimir Onoprienko <vonopr@ya.ru>
2021-08-03 18:21:28 +0300

    Provide hint to determine the name of the project
    The name of the project can be extracted from the URL of the project's
    repo.
    
    If 'title' is missing in the "gitlog.yaml" file
      then the script will try to guess the project's name by its URL.
    
    Note:
      The order of --metadata-file arguments passed to pandoc is important!
    
      The script will write the guessed project name into a temporary file
      "project.yaml". The .yaml files are provided to pandoc with
      --metadata-file option.
    
      If both the files contain title meatadata the second file is taken
      into account.
    
    Note:
      This commit breaks down the compatability with Windows OS due to
        `a=$(b)` style commands

--- log2pdf.sh
+++ log2pdf.sh
@@ -1,7 +1,15 @@
+remote=$(git remote)
+url=$(git remote get-url $remote)
+project=$(basename $url .git)
+
 echo '```' > gitlog.md
 git log --reverse --stat --format='```%n## %s%n```%n%n%H%n%ai%n%an <%ae>%n```%n  %b%nFiles changed: %n```' >> gitlog.md
 echo '```' >> gitlog.md
 pandoc -f markdown-raw_tex gitlog.md  -o gitlog.md # use pandoc filter "markdown-raw_tex" to escape backslashes in commit messages
+
+echo 'The changelog of log2pdf project generated from git log.'\
+'\textit{\hyperref[toc]'\
+'{The table of contents is at the end of the document.}}' > intro.md
 
 echo '\newpage' > toc.md
 echo '\tableofcontents\label{toc}' >> toc.md
@@ -9,8 +17,15 @@ echo '\tableofcontents\label{toc}' >> toc.md
 dos2unix gitlog.md
 dos2unix toc.md
 
-pandoc -s  gitlog.yaml gitlog.md toc.md  -o gitlog.tex
-latexmk -pdf -pdfxe -f  gitlog.tex
+echo '
+---
+title: Changelog of '$project' project
+...' > project.yaml
 
+pandoc -s --metadata-file=project.yaml gitlog.yaml  intro.md gitlog.md toc.md -o gitlog.tex
+latexmk -pdf -pdfxe -f gitlog.tex
+
+rm intro.md
 rm gitlog.md
 rm toc.md
+rm project.yaml
