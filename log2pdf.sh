8bf90976307edcbb36a1a4cd5c4a1fe4b984540f
Vladimir Onoprienko <vonopr@ya.ru>
2021-08-04 13:42:52 +0300

    Add link to URL of documented project
    If the first remote repo (aka origin) of the documented local repo
    follows https protocol then use this URL as a link inside the generated
    document.
    
    If the remote repo follows SSH protocal then deduce the https-URL.
    
    Should work at least for GitHub and GitLab servers.
    
    Note:
      I turn on colorlinks with `hypersetup`. As for now I need only
      coloured URL-links. So, I set the color of the rest types of links to
      black.
    
    Note:
      There is a mess with which type of link `\href` produces:
        it may be either a urllink or filelink.
      As for now there are no links to files I can set the colors of
      both types to blue.

--- log2pdf.sh
+++ log2pdf.sh
@@ -1,13 +1,14 @@
 remote=$(git remote)
 url=$(git remote get-url $remote)
 project=$(basename $url .git)
+https_url=$(url1=${url/:/"/"}; url2=${url1/git@/https://};echo $url2)
 
 echo '```' > gitlog.md
 git log --reverse --stat --format='```%n## %s%n```%n%n%H%n%ai%n%an <%ae>%n```%n  %b%nFiles changed: %n```' >> gitlog.md
 echo '```' >> gitlog.md
 pandoc -f markdown-raw_tex gitlog.md  -o gitlog.md # use pandoc filter "markdown-raw_tex" to escape backslashes in commit messages
 
-echo 'The history of' $project 'project generated from git log.'\
+echo 'The history of \href{'$https_url'}{'$project'} project generated from git log.'\
 '\textit{\hyperref[toc]'\
 '{The table of contents is at the end of the document.}}' > intro.md
 
